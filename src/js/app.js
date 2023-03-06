7d7b57edea802e3fc20d0420094e0dee132df48f
Denis Kravchuk <deniskrv@gmail.com>
2023-03-07 00:17:34 +0300

    create hero section

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import * as flsFunctions from './modules/functions.js'
-import {burger} from "./modules/functions.js";
+import {burger, dropDown} from "./modules/functions.js";
 
 flsFunctions.isWebp()
 burger()
+dropDown()
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,32 +1,52 @@
 export function isWebp() {
-  function testWebP(callback) {
+    function testWebP(callback) {
 
-    let webP = new Image();
-    webP.onload = webP.onerror = function () {
-      callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
-  testWebP(function (support) {
+        let webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
 
-    let className = support === true ? 'webp' : 'no-webp'
-    document.documentElement.classList.add(className)
-  });
+    testWebP(function (support) {
+
+        let className = support === true ? 'webp' : 'no-webp'
+        document.documentElement.classList.add(className)
+    });
 }
+
 export function burger() {
-  const burger = document.querySelector('.menu__icon')
-  const menu = document.querySelector('.menu')
-  const body = document.body
+    const burger = document.querySelector('.menu__icon')
+    const menu = document.querySelector('.menu')
+    const body = document.body
 
-  if(burger && menu){
-    burger.addEventListener('click', ()=>{
-      burger.classList.toggle('--active')
-      menu.classList.toggle('--active')
-      body.classList.toggle('lock')
-    })
-  }
+    if (burger && menu) {
+        burger.addEventListener('click', () => {
+            burger.classList.toggle('--active')
+            menu.classList.toggle('--active')
+            body.classList.toggle('lock')
+        })
+    }
 }
 
+export function dropDown() {
+    const filter = document.querySelector('.filter')
+
+    if (filter) {
+        const items = document.querySelectorAll('.block-filter')
+
+        items.forEach(item => {
+            item.addEventListener('click', e => {
+                item.querySelector('.block-filter__dropdown').classList.toggle('--active')
+                item.querySelector('.block-filter__icon').classList.toggle('--active')
+
+                if (e.target.classList.contains('block-filter__item')) {
+                    item.querySelector('.block-filter__value').textContent = e.target.textContent
+                }
+            })
+        })
+    }
+}
 
 
 
