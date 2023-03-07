97bb8b41e9a4a57b357f10d5c6f8e991caa2f680
Denis Kravchuk <deniskrv@gmail.com>
2023-03-07 18:29:09 +0300

    create popular section

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from './modules/functions.js'
 import {burger, dropDown} from "./modules/functions.js";
+import {slider} from "./modules/slider.js";
 
 flsFunctions.isWebp()
 burger()
 dropDown()
+slider()
